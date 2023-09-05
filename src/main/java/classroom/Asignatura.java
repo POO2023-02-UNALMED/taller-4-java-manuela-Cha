0dc2ff1ca113e286dfaad46a9d4374564a3e4941
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-05 09:23:29 -0500

    Update Asignatura.java

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,11 +10,10 @@ public class Asignatura {
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
     }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
+    public Asignatura(double codigoInterno,Tipo tipo) {
+        this("Sin nombre", (int) codigoInterno, 0,tipo);
     }
 
     public Asignatura(int codigoExterno,Tipo tipo) {
