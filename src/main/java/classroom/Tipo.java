f6b61e5912e65008199ba7be47f66df46f77a0a0
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-08 15:31:47 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -3,8 +3,9 @@ package classroom;
 public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
-	private int codigo;
-	private String nombre;
+	private final int codigo;
+	private final String nombre;
+	
 	public Tipo(int codigo,String nombre) {
 		this.codigo = codigo;
 		this.nombre = nombre;
