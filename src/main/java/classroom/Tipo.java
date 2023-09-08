6dd82b007d3eed8b482e75a9cc749173ac1edb8e
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-08 18:35:06 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -4,10 +4,10 @@ public enum Tipo {
 	DISCIPLINAR(10) ,FUNDAMENTACION(20),ELECTIVA(30);
 	
 	private final int codigo;
-	//private final String nombre;
+	private final String nombre;
 	
-	private Tipo(int codigo) {
+	private Tipo(int codigo, String nombre) {
 		this.codigo = codigo;
-		//this.nombre = nombre;
+		this.nombre = nombre;
 	}
 }
